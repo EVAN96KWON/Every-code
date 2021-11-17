5c649272b7ab48b54da8a18087721a77f959daed
Dohyeon_17 <pak981116@gmail.com>
2021-11-17 17:38:08 +0900

    second

---
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/AnalysisActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20170419.everydaymath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analysis)
+    }
+}
--- app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/MainActivity.kt
@@ -1,11 +1,34 @@
 package kr.ac.kumoh.s20170419.everydaymath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20170419.everydaymath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
+
+        view.main1131.setOnClickListener {
+            val intent = Intent(this, TestActivity::class.java)
+            startActivity(intent)
+        }
+
+        view.main1121.setOnClickListener {
+            val intent = Intent(this, AnalysisActivity::class.java)
+            startActivity(intent)
+        }
+
+        view.main1141.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
