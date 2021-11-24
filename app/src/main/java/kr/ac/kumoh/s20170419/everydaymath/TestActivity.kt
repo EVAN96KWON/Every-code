81109ab604a1f11ddf64a48e8b38244ccb66917a
Dohyeon_17 <pak981116@gmail.com>
2021-11-24 20:31:13 +0900

    3

--- app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/TestActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/TestActivity.kt
@@ -2,10 +2,22 @@ package kr.ac.kumoh.s20170419.everydaymath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20170419.everydaymath.databinding.ActivityTestBinding
+import kotlin.random.Random
 
 class TestActivity : AppCompatActivity() {
+    private lateinit var view : ActivityTestBinding
+
+    private var num1 = (1..10).random()
+    private var num2 = (1..10).random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        view = ActivityTestBinding.inflate(layoutInflater)
+
+        setContentView(view.root)
+
+        view.testLine1.setText("${num1}")
+        view.testLine2.setText("${num2}")
+
     }
 }
