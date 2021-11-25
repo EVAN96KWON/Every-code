634a76fe574f7e2209bf7192a35681cffb31731b
heonjin <87924725+EVAN96KWON@users.noreply.github.com>
2021-11-25 16:54:09 +0900

    Update AnalysisActivity.kt
    참고할것

--- app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/AnalysisActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/AnalysisActivity.kt
@@ -9,3 +9,5 @@ class AnalysisActivity : AppCompatActivity() {
         setContentView(R.layout.activity_analysis)
     }
 }
+// 참고 MPAndroidChart
+// https://rollinkg.dev/p/android-studio-mpandroidchart-%EB%A7%89%EB%8C%80-%EA%B7%B8%EB%9E%98%ED%94%84-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0/
