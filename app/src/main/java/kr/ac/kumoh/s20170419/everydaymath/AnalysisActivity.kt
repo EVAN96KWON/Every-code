84cba35527639e9d46213a748b3cf7a36854a1ff
EVAN9416KWON <koun012@naver.com>
2021-11-28 00:18:41 +0900

    main1111에 유저 정보 불러오고 표시

--- app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/AnalysisActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/AnalysisActivity.kt
@@ -2,10 +2,16 @@ package kr.ac.kumoh.s20170419.everydaymath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20170419.everydaymath.databinding.ActivityAnalysisBinding
 
 class AnalysisActivity : AppCompatActivity() {
+    private lateinit var view: ActivityAnalysisBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_analysis)
+        view = ActivityAnalysisBinding.inflate(layoutInflater)
+        setContentView(view.root)
+
+
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/everydaymath/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.s20170419.everydaymath
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
 import kr.ac.kumoh.s20170419.everydaymath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-
         view = ActivityMainBinding.inflate(layoutInflater)
         setContentView(view.root)
+
+        val sps = PreferenceManager.getDefaultSharedPreferences(this)
+        val userNickname = sps.getString("user_nickname", "defalut_nickname")
+        val userGrade = sps.getString("user_grade", "")
+        val userProblemsNums = sps.getString("user_problems_nums", "")
+        val appTheme = sps.getBoolean("app_theme", false)
+
+        view.main1111.text = "$userGrade ${userNickname}님 안녕하세요!\n" +
+                "현재 문제수 $userProblemsNums, 다크 테마 $appTheme 입니다."
 
         view.main1131.setOnClickListener {
             val intent = Intent(this, TestActivity::class.java)
